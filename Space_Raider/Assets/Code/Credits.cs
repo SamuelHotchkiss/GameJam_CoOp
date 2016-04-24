70d223ceed80b2c0fe66264b3e1ae6ada21a496f
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2016-04-23 22:10:18 -0400

    SOUNDS and FONTS and MENUS

---
+++ Space_Raider/Assets/Code/Credits.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Credits : MonoBehaviour 
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
